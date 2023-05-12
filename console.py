e7ab71c76818043afe88b7c2a78fa27889708ef2
Huming-bird <sonugaahmed@gmail.com>
2023-05-12 12:31:12 -0700

    tracking changes

--- console.py
+++ console.py
@@ -2,6 +2,8 @@
 """ This module holds our console source code """
 
 import cmd
+from models.base_model import BaseModel
+import json
 
 
 class HBNBCommand(cmd.Cmd):
@@ -22,6 +24,27 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """ Overriding base class method to do nothing """
         pass
+
+    def do_create(self, arg):
+        """ This console method will create class objects """
+
+        # checks if arg is present
+        if arg:
+            if arg == "BaseModel":
+                new_object = BaseModel()
+                new_object.save()
+
+                print(new_object.id)
+            else:
+                print("** class doesn't exist **")
+        else:
+            print("** class name is missing **")
+
+    def do_show(self, arg):
+        """ This method prints the str rep of an object """
+
+        print(arg)
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
